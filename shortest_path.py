81c9a5766c3e9afbe01197fbac55ca0ed2a5989c
Ramon Wijnands <ramon.wijnands@nobleo.nl>
2023-09-15 10:08:21 +0200

    Implement dijkstra

--- shortest_path.py
+++ shortest_path.py
@@ -1,8 +1,50 @@
+from collections import deque
 from heapq import heappush, heappop
 
 import numpy as np
 
 from ...constants import RIGHT, UP, LEFT, DOWN
+
+
+def dijkstra(start, grid):
+    assert is_on_grid(start, grid.shape)
+
+    queue = deque()
+
+    dist = np.full_like(grid, np.iinfo(int).max, dtype=int)
+    dist[start[0], start[1]] = 0
+
+    queue.append(start)
+
+    while queue:
+        current = queue.popleft()
+        # print(f'inspecting {current}')
+
+        new_dist = dist[current[0], current[1]] + 1
+        for neighbor in neighbors(current, grid):
+            if new_dist < dist[neighbor[0], neighbor[1]]:
+                dist[neighbor[0], neighbor[1]] = new_dist
+                # print(f'pushing {neighbor}')
+                queue.append(neighbor)
+
+    return dist
+
+
+def neighbors(position, grid):
+    if position[0] > 0:
+        yield from _possible_neighbor(position + LEFT, grid)
+    if position[1] > 0:
+        yield from _possible_neighbor(position + DOWN, grid)
+    if position[0] < grid.shape[0] - 1:
+        yield from _possible_neighbor(position + RIGHT, grid)
+    if position[1] < grid.shape[1] - 1:
+        yield from _possible_neighbor(position + UP, grid)
+
+
+def _possible_neighbor(pos, grid):
+    # print(f'collision: {self.grid[pos[0], pos[1]]}')
+    if not grid[pos[0], pos[1]]:
+        yield pos
 
 
 def shortest_path(start, goal, grid):
--- test_shortest_path.py
+++ test_shortest_path.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from .shortest_path import shortest_path
+from .shortest_path import shortest_path, dijkstra
 
 
 def test_shortest_path():
@@ -33,3 +33,15 @@ def test_shortest_path_detour():
         [0, 0, 0, 0],
     ], dtype=bool)).T
     assert 7 == shortest_path(start, goal, grid)
+
+
+def test_dijkstra():
+    start = np.array([0, 3])
+    grid = np.flipud(np.array([
+        [0, 0, 1, 0],
+        [0, 1, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+    ], dtype=bool)).T
+    dist = dijkstra(start, grid)
+    assert dist[3, 3] == 7
