6c5a0d434a0342642dffd8653dc6ee6a24971442
Ramon Wijnands <ramon.wijnands@nobleo.nl>
2023-09-30 17:55:11 +0200

    Further inline vector_add

--- utils.py
+++ utils.py
@@ -1,37 +1,23 @@
-from typing import Tuple
-
-UP = (0, 1)
-DOWN = (0, -1)
-LEFT = (-1, 0)
-RIGHT = (1, 0)
-
-
 def is_on_grid(pos, grid_size):
     return 0 <= pos[0] < grid_size[0] and 0 <= pos[1] < grid_size[1]
-
-
-def vector_add(a: Tuple, b: Tuple):
-    assert len(a) == len(b) == 2
-    return a[0] + b[0], a[1] + b[1]
-    # return tuple(map(add, a, b))
 
 
 def neighbors(i: int, j: int, grid):
     # assert isinstance(i, int), type(i)
     # assert isinstance(j, int), type(j)
     if i > 0:
-        pos = vector_add((i, j), LEFT)
+        pos = i - 1, j
         if not grid[pos[0], pos[1]]:
             yield pos
     if j > 0:
-        pos = vector_add((i, j), DOWN)
+        pos = i, j - 1
         if not grid[pos[0], pos[1]]:
             yield pos
     if i < grid.shape[0] - 1:
-        pos = vector_add((i, j), RIGHT)
+        pos = i + 1, j
         if not grid[pos[0], pos[1]]:
             yield pos
     if j < grid.shape[1] - 1:
-        pos = vector_add((i, j), UP)
+        pos = i, j + 1
         if not grid[pos[0], pos[1]]:
             yield pos
