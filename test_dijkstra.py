1c37c336d8694076c567a59175f2698854af5fc9
Ramon Wijnands <ramon.wijnands@nobleo.nl>
2023-10-02 09:03:31 +0200

    Make the dijkstra benchmark more realistic

--- test_dijkstra.py
+++ test_dijkstra.py
@@ -18,14 +18,7 @@ def test_dijkstra():
 
 
 def test_benchmark_dijkstra(benchmark):
-    start = (0, 3)
-    grid = np.flipud(np.array([
-        [0, 0, 1, 0],
-        [0, 1, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-    ], dtype=bool)).T
+    start = (3, 5)
+    grid = np.zeros((16, 16), dtype=bool)
     dist = benchmark(dijkstra, start, grid)
     print_array(dist)
-    # assert dist[3 * grid.shape[1] + 3] == 7
-    assert dist[3, 3] == 7
